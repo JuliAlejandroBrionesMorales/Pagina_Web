fa3655d8ecc7b9a62d68c51b2befdb5a70c7aecf
Julio Briones <juliobriones842@gmail.com>
2024-08-25 10:48:14 +0200

    creacion py

---
+++ pagina.py
@@ -0,0 +1,54 @@
+# LIBRERIAS
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+# CONFIGURACION DE LA PÁGINA
+st.set_page_config(page_title='Pajuelo Brand', layout='wide')
+
+
+# CSS PARA CAMBIAR COLOR DE FONDO
+page_bg_color = '''
+<style>
+    .stApp {
+        background-color: #E0E0E0; /* Gris más oscuro */
+    }
+    .title {
+        color: #ffffff;
+        text-align: center;
+        font-size: 60px;
+        font-family: 'Arial', sans-serif;
+        background: linear-gradient(to right, #0f2027, #203a43, #2c5364);
+        padding: 20px;
+        border-radius: 10px;
+    }
+    .content-box {
+        background-color: #ffffff; /* Fondo blanco para el contenido */
+        border: 2px solid #d1d1d1; /* Borde gris claro */
+        border-radius: 10px; /* Bordes redondeados */
+        padding: 20px; /* Espaciado interno */
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); /* Sombra sutil */
+        font-family: 'Arial', sans-serif; /* Fuente consistente */
+    }
+    .highlight {
+        color: #FFA500; /* Color naranja para resaltar puntos clave */
+        font-weight: bold;
+    }
+    .section-title {
+        font-size: 24px; /* Tamaño de fuente para los títulos de sección */
+        border-bottom: 2px solid #FFA500; /* Línea naranja debajo del título */
+        padding-bottom: 10px; /* Espaciado debajo del título */
+        margin-bottom: 20px; /* Espaciado debajo del título */
+    }
+</style>
+'''
+
+st.markdown(page_bg_color, unsafe_allow_html=True)
+
+
+custom_title = '''
+    <div class="title">
+        PRESENTACIÓN
+    </div>
+    '''
+st.markdown(custom_title, unsafe_allow_html=True)
